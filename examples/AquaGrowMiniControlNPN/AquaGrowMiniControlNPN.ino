29e4e8956f611aae2fedf433816456982b4b7024
MajorMadness <mstravendev@gmail.com>
2015-02-11 12:22:49 +0100

    Hinweise zur verwendung meiner Scripte in anderen Projekten hinzugefügt

--- examples/AquaGrowMiniControlNPN/AquaGrowMiniControlNPN.ino
+++ examples/AquaGrowMiniControlNPN/AquaGrowMiniControlNPN.ino
@@ -1,3 +1,19 @@
+/*
+  AquaGrow Programmcode zur Steuerung von Aquarien
+  Copyright (C)2013-2015 AquaGrow - Moritz Stapelfeldt. All right reserved
+  
+  Der Code wurde entwickelt um Aquarien zu automatisieren und zu steuern. Er ist frei zugänglich und unterliegt dem Gedanken des OpenSource. Jeder kann ihn kostenfrei verwenden, kopieren, ganz oder teilweise solange die verwendeten Codezeilen als Fremdcode Kenntlich gemacht wurden und ein Link zum Orginal und/oder zur Webseite http://www.Aqua-Grow.de erhalten bleibt.
+  
+  Ich, als Urheber des Quellcodes übernehme keine Garantie auf die funktion des Quellcodes im ganzen oder teilen. Für evt Bugfixes und Updates besuche bitte http://www.Aqua-Grow.de und/oder https://github.com/majormadness
+  
+  Wenn veränderungen, modifikationen und/oder verbesserungen am Code vorgenommen werden bitte Ich um mitteilung unter info@aqua-grow.de
+
+  This Software is free software; you can redistribute it and/or modify it under the terms of the CC BY-NC-SA 3.0 license.
+
+  Commercial use of this Software requires you to buy a license that will allow commercial use. This includes using the Software, modified or not, as a tool to sell products.
+
+  The license applies to all part of the Software including the examples and tools supplied with the Software.
+*/
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <RTClib.h>
